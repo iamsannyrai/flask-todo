5677e3cd34b7c43dbafe96e9990097b677258777
@iamsannyrai <iam.sannyrai@gmail.com>
2021-08-02 21:25:13 +0545

    adds migration

--- application_factory.py
+++ application_factory.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from extensions import db, ma
+from extensions import db, ma, migrate
 from todo_app.todo import todo_app
 
 
@@ -9,5 +9,6 @@ def create_app(config_file):
     flask_app.config.from_object(config_file)
     flask_app.register_blueprint(todo_app, url_prefix="/api")
     db.init_app(flask_app)
+    migrate.init_app(flask_app, db)
     ma.init_app(flask_app)
     return flask_app
--- extensions.py
+++ extensions.py
@@ -1,5 +1,7 @@
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
 ma = Marshmallow()
+migrate = Migrate()
